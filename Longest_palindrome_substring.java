cd1a849b0954b174510b90c69d52483fdb2b9eea
Advait Churi <advaitchuri@Advaits-MacBook-Pro.local>
2024-08-22 22:26:38 -0700

    DP-6

---
+++ Longest_palindrome_substring.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(n^2)
+// Space Complexity : O(n^2)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int max = 0;
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j <= i; j++){
+                if( (s.charAt(i) == s.charAt(j)) && ((i - j - 1 < 2) || (dp[i-1][j+1])) ){
+                    dp[i][j] = true;
+                }
+                if(dp[i][j] == true){
+                    if(i - j + 1 > max){
+                        max = i - j + 1;
+                        start = j;
+                        end = i;
+                    }
+                }
+            }
+        }
+        return s.substring(start, end+1);
+    }
+}
